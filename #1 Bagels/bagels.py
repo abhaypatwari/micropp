9529f05000bc9e572865811633c5048fe1309941
Abhay Patwari <patwari.abhay.ap@gmail.com>
2021-07-06 20:18:55 +0530

    added restart() functionality
    and made a few other changes

--- #1 Bagels/bagels.py
+++ #1 Bagels/bagels.py
@@ -1,5 +1,13 @@
-import random
+def restart():
+        import sys
+        print("argv was",sys.argv)
+        print("sys.executable was", sys.executable)
+        print("restarting now")
+        import os
+        os.execv(sys.executable, ['python'] + sys.argv)
+        
 def getmatch():
+    import random
     num = random.randint(0,999)
     if num <= 9:
         return '00'+str(num)
@@ -8,7 +16,7 @@ def getmatch():
 
     return str(num)
 
-print("I am thinking of a 3-digit number. Try to guess what it is.\nHere are some clues:\nWhen I say:\tThat means:\n  Pico \t\tOne digit is correct but in the wrong position.\n  Fermi \tOne digit is correct and in the right position.\n  Bagels \tNo digit is correct.\nI have thought up a number.\nYou have 10 guesses to get it.")
+print("\nI am thinking of a 3-digit number. Try to guess what it is.\nHere are some clues:\nWhen I say:\tThat means:\n  Pico \t\tOne digit is correct but in the wrong position.\n  Fermi \tOne digit is correct and in the right position.\n  Bagels \tNo digit is correct.\nI have thought up a number.\nYou have 10 guesses to get it.")
 match = getmatch()
 guess_count=0
 
@@ -16,17 +24,36 @@ while True:
     print('Guess #{}'.format(guess_count+1))
     guess = input('> ')
     guess_count +=1
-    if guess_count==10:
-        print('You ran out of guesses')
-        break
     
-    if len(guess) > 3 or not guess.isdecimal():
+    if len(guess)>3 or not guess.isdecimal() or len(guess)<3:
             print('DO YOU NOT UNDERSTAND WHAT A 3 DIGIT NUMBER IS YOU FUCKING MORON?!')
             guess_count-=1
             continue
-            
+        
+    if guess_count==10:
+        print('You ran out of guesses :(\nThe number was {}'.format(match))
+        while True:
+            new_game = input('Do you want to play again?[y/n] >')
+            if new_game=='y' or new_game=='Y':
+                restart()
+            if new_game=='n' or new_game=='N':
+                print('Thanks for playing!')
+                break
+            else:
+                print('Invalid input')
+        break
+    
     if guess == match:
-        print('Congratulations! You got it!')
+        print('***Congratulations! You got it!***')
+        while True:
+            new_game = input('Do you want to play again?[y/n] >')
+            if new_game=='y' or new_game=='Y':
+                restart()
+            if new_game=='n' or new_game=='N':
+                print('Thanks for playing!')
+                break
+            else:
+                print('Invalid input')
         break
 
     i=0
