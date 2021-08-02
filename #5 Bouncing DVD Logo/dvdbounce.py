389fa19f37456440c03912fe18c734139351778c
Abhay Patwari <patwari.abhay.ap@gmail.com>
2021-08-02 10:20:41 +0530

    added corner bounces count; fixed blacked text

--- #5 Bouncing DVD Logo/dvdbounce.py
+++ #5 Bouncing DVD Logo/dvdbounce.py
@@ -9,15 +9,15 @@ def clear():
 dim = bext.size()
 width = dim[0]
 height = dim[1]
-#print(width,"  ",height)
+width += -1
 x_init = random.randint(0,width-1)
 y_init = random.randint(0,height-1)
 x_speed = 1
 y_speed = 1
-bext.fg('random')
+fg_colors = ['red', 'green', 'yellow', 'blue', 'purple', 'cyan', 'white']
+bext.fg(random.choice(fg_colors))
 bext.goto(x_init,y_init) 
-print('DVD')
-clear()
+cbounce = 0
 
 while True:
     x = x_init + x_speed
@@ -26,12 +26,21 @@ while True:
         x_init = x
         y_init = y
         x_speed = x_speed*-1
-        bext.fg('random')
-    elif y == height-1 or y == 0:
+        bext.fg(random.choice(fg_colors))
+    if y == height-1 or y == 0:
         x_init = x
         y_init = y
         y_speed = y_speed*-1
-        bext.fg('random')
+        bext.fg(random.choice(fg_colors))
+    if x == width-1 and y == 0:
+        cbounce+=1
+    if x == width-1 and y == height-1:
+        cbounce+=1
+    if x == 0 and y == height-1:
+        cbounce+=1
+    if x == 0 and y == 0:
+        cbounce+=1
+    print('>CORNER BOUNCES={}'.format(cbounce))    
     bext.goto(x,y) 
     print('DVD')
     x_init = x
